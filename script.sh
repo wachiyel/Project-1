5aedca52edb088b0dc2ee38c960db27c9b2a90a6
wachiyel <160839976+wachiyel@users.noreply.github.com>
2024-02-24 10:10:21 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# initialize dir path
+myFiles=~/myFiles
+
+# ensure dir exists
+mkdir -p "$myFiles"
+
+# prefix
+
+myName="Lucy"
+fileNumberTracker="$myFiles/.fileNumber"
+
+if [ -f "$fileNumberTracker" ]; then
+	fileNumber=$(< "$fileNumberTracker")
+else
+	touch "$fileNumberTracker"
+	fileNumber=1;
+fi
+
+
+cd "$myFiles" || exit
+
+for ((i = 1; i <=25; i++)); do
+	touch "${myName}${fileNumber}"
+	((fileNumber++))
+done
+
+echo "$fileNumber" > "$fileNumberTracker"
+
+ls -la "$myFiles"
